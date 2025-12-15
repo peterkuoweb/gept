739874fd8d20d13dc003e1a6b2b881d2c0302582
peterkuotw <96007877+peterkuoweb@users.noreply.github.com>
2025-12-15 20:56:27 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // This is critical for GitHub Pages (relative paths)
+  // IMPORTANT: Replace 'gept' with your repository name if it changes
+  base: '/gept/', 
   build: {
     outDir: 'dist',
   }
